fd7f4503a5f508e7acc5febc1f7b7dfca4a77d48
Serhii911 <serj.roshen@gmail.com>
2022-11-30 07:19:13 +0200

    Update NameClass.java (task 1)

--- src/NameClass.java
+++ src/NameClass.java
@@ -24,6 +24,7 @@ public class NameClass {
                 result.append(stringFormat);
             }
         }
+        result.setLength(result.length() - 2);
         return result.toString();
     }
 
