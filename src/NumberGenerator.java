284bc2e5a5dce94fc4d9dc28f3e2beafdb6ce0a2
Serhii911 <serj.roshen@gmail.com>
2022-12-01 18:30:05 +0200

    added NumberGenerator.java

--- src/NumberGenerator.java
+++ src/NumberGenerator.java
@@ -5,6 +5,7 @@ public class NumberGenerator {
         randomStream(25214903917L, 11, (long) Math.pow(2d, 48d), 0);
     }
     public static Stream<Long> randomStream(long a, long c, long m, long seed){
+
         return Stream.iterate(seed, x -> (a * x + c) % m);
     }
 }
