9824f08c30efda8d289d1c033cc90f90e0a0839f
Serhii911 <serj.roshen@gmail.com>
2022-12-02 15:50:32 +0200

    Update MixerElements.java

--- src/MixerElements.java
+++ src/MixerElements.java
@@ -1,17 +1,16 @@
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import java.util.stream.Stream;
 
 public class MixerElements {
 
-    public static <T> void zip(Stream<T> first, Stream<T> second) {
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
         List<T> list = new ArrayList<>();
-                first.forEach(x -> list.add(x));
-                second.forEach(x -> list.add(x));
-                Collections.shuffle(list);
-        System.out.println(list);
+        first.forEach(list::add);
+        second.forEach(list::add);
+        Collections.shuffle(list);
+        return list.stream();
     }
 
     public static void main(String[] args) {
@@ -24,8 +23,6 @@ public class MixerElements {
         listTwo.add(1);
         listTwo.add(2);
         listTwo.add(3);
-        listTwo.add(4);
-        zip(list.stream(), listTwo.stream());
     }
 
 }
